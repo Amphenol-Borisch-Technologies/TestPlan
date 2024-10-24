bbe48bdb2ec75c1f4372b452f63569df15c0f33d
Phillip Smelt <phils@borisch.com>
2024-10-24 14:52:29 -0400

    10/24/24

--- TestPlan/TestOperations/T-20.cs
+++ TestPlan/TestOperations/T-20.cs
@@ -4,8 +4,11 @@ using System.Windows.Forms;
 using ABT.Test.Exec;
 using ABT.Test.Exec.AppConfig;
 using ABT.Test.Exec.InstrumentDrivers;
-using ABT.Test.Exec.InstrumentDrivers.PowerSupplies;
 using ABT.Test.Exec.InstrumentDrivers.Interfaces;
+using ABT.Test.Exec.InstrumentDrivers.Multifunction;
+using ABT.Test.Exec.InstrumentDrivers.MultiMeters;
+using ABT.Test.Exec.InstrumentDrivers.Oscilloscopes;
+using ABT.Test.Exec.InstrumentDrivers.PowerSupplies;
 using ABT.Test.UUT.Instruments;
 
 namespace ABT.Test.UUT.TestOperations {
@@ -34,6 +37,12 @@ namespace ABT.Test.UUT.TestOperations {
                 ClassName: nameof(MeasurementCustom),
                 CancelNotPassed: true,
                 Arguments: "NotApplicable"));
+            _ = ((MSO_3014_IVI_COM)TestPlan.Only.Instruments[IA.MSO]).Diagnostics();
+            _ = ((PS_E3634A_SCPI_NET)TestPlan.Only.Instruments[IA.V28_IN]).Diagnostics();
+            _ = ((PS_E3649A_SCPI_NET)TestPlan.Only.Instruments[IA.SEAL]).Diagnostics();
+            _ = ((MM_34401A_SCPI_NET)TestPlan.Only.Instruments[IA.MM]).Diagnostics();
+            _ = ((MSMU_34980A_SCPI_NET)TestPlan.Only.Instruments[IA.MSMU]).Diagnostics();
+
             ((PS_E3634A_SCPI_NET)TestPlan.Only.Instruments[IA.V28_IN]).Set(3.3, 0.1, 7, STATES.ON);
             ((PS_E3649A_SCPI_NET)TestPlan.Only.Instruments[IA.SEAL]).Set(OUTPUTS2.OUTput1, 1, 0.1, 7, STATES.ON);
             ((PS_E3649A_SCPI_NET)TestPlan.Only.Instruments[IA.SEAL]).Set(OUTPUTS2.OUTput2, 2, 0.1, 7, STATES.ON);
