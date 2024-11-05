f85045d6c98cca5bc265028cb78f3231707294d4
Phillip Smelt <phils@borisch.com>
2024-11-05 09:53:36 -0500

    11/5/24

--- Diagnostics/InstrumentDrivers/ID.cs
+++ Diagnostics/InstrumentDrivers/ID.cs
@@ -8,7 +8,7 @@ namespace ABT.TestExec.Tests.UUT.InstrumentsDrivers {
     internal static class ID {
         internal static PS_E3634A_SCPI_NET   V28_IN = ((PS_E3634A_SCPI_NET)Lib.TestLib.InstrumentDrivers["PS3_E3634A"]);
         internal static PS_E3649A_SCPI_NET   SEAL   = ((PS_E3649A_SCPI_NET)Lib.TestLib.InstrumentDrivers["PS1ε2_E3649A"]);
-        internal static MSO_3014_IVI_COM     MSO    = ((MSO_3014_IVI_COM)Lib.TestLib.InstrumentDrivers["MSO1_3014"]);
+        // internal static MSO_3014_IVI_COM     MSO    = ((MSO_3014_IVI_COM)Lib.TestLib.InstrumentDrivers["MSO1_3014"]);
         internal static MSMU_34980A_SCPI_NET MSMU   = ((MSMU_34980A_SCPI_NET)Lib.TestLib.InstrumentDrivers["MSMU1_34980A"]);
         // internal static MM_34401A_SCPI_NET   MM     = ((MM_34401A_SCPI_NET)TestData.InstrumentDrivers["MM1_34401A"]);
     }
