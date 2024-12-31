65d9e1dfb45e0683bb2e30bab90b0f4b0a62dbe7
Phillip Smelt <phils@borisch.com>
2024-12-31 08:23:02 -0500

    12./31/24

--- Diagnostics.cs
+++ Diagnostics.cs
@@ -132,7 +132,7 @@ namespace ABT.Test.TestPlans.Diagnostics {
         }
 
         protected override async Task<String> MeasurementRun(Method method) {
-            Type type = Type.GetType($"{TestSelection.TestSpace.NamespaceRoot}.{TestIndex.TestOperation.NamespaceTrunk}.{TestIndex.TestGroup.Class}");
+            Type type = Type.GetType($"{TestLib.TestLib.testDefinition.TestSpace.NamespaceRoot}.{TestIndex.TestOperation.NamespaceTrunk}.{TestIndex.TestGroup.Class}");
             // NOTE:  Will only seek invocable measurement methods in class TestMeasurements that are defined as TestMeasurement IDs in App.config & and are part of a Group.
             MethodInfo methodInfo = type.GetMethod(method.Name, BindingFlags.Static | BindingFlags.NonPublic);
             // NOTE:  Invocable measurement methods in class TestMeasurements, defined as TestMeasurement IDs in App.config, must have signatures identical to "internal static String MethodName()",
