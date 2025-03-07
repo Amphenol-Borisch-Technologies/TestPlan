ec676bdcb18845dcd76db32dd273eb12677814b7
Phillip Smelt <phils@borisch.com>
2025-03-07 10:07:42 -0500

    3/7/25

--- InstrumentDrivers/InstrumentAliases.cs
+++ InstrumentDrivers/InstrumentAliases.cs
@@ -1,5 +1,7 @@
 namespace ABT.Test.TestPlans.Diagnostics.InstrumentDrivers {
+    using ABT.Test.TestLib.InstrumentDrivers.Generic;
     using ABT.Test.TestLib.InstrumentDrivers.Multifunction;
+    using ABT.Test.TestLib.InstrumentDrivers.MultiMeters;
     using ABT.Test.TestLib.InstrumentDrivers.Oscilloscopes;
     using ABT.Test.TestLib.InstrumentDrivers.PowerSupplies;
     
@@ -8,5 +10,7 @@ namespace ABT.Test.TestPlans.Diagnostics.InstrumentDrivers {
         internal static PS_E3649A_SCPI_NET P12V_N12V = ((PS_E3649A_SCPI_NET)(TestLib.Data.InstrumentDrivers["PS1ε2_E3649A"]));
         internal static PS_E3634A_SCPI_NET P28V = ((PS_E3634A_SCPI_NET)(TestLib.Data.InstrumentDrivers["PS3_E3634A"]));
         internal static MSO_3014_IVI_COM MSO = ((MSO_3014_IVI_COM)(TestLib.Data.InstrumentDrivers["MSO1_3014"]));
+        internal static MM_34401A_SCPI_NET MM = ((MM_34401A_SCPI_NET)(TestLib.Data.InstrumentDrivers["MM1_34401A_SCPI_NET"]));
+        internal static SCPI_NET WG = ((SCPI_NET)(TestLib.Data.InstrumentDrivers["WG1_33120A"]));
     }
 }
--- TestPlan/InstrumentsTestPlan.cs
+++ TestPlan/InstrumentsTestPlan.cs
@@ -23,7 +23,7 @@
             Debug.Assert(TestGroup(Classname: "SwitchingUnits", Description: "Keysight 34980As.", CancelNotPassed: "false", Independent: "true", Methods: "MSMU_34980A"));
             Debug.Assert(MethodCustom(Name: "MSMU_34980A", Description: "Keysight 34980A Multifunction Switch/Measurement Units.", CancelNotPassed: "false"));
             if (Data.testSequence.IsOperation) {
-                Debug.Assert(TestOperation(NamespaceTrunk: "InstrumentsTestPlan", ProductionTest: "true", Description: "Manufacturer\'s self-tests + ABT tests of TestPlanDefinition\'s InstrumentsTestPlan.", TestGroups: "SwitchingUnits|PowerSupplies|DigitalMultiMeters|Oscilloscopes"));
+                Debug.Assert(TestOperation(NamespaceTrunk: "InstrumentsTestPlan", ProductionTest: "true", Description: "Manufacturer\'s self-tests + ABT tests of TestPlanDefinition\'s InstrumentsTestPlan.", TestGroups: "SwitchingUnits|PowerSupplies|DigitalMultiMeters|Oscilloscopes|WaveformGenerators"));
                 Debug.Assert(TestGroupPrior(Classname: NONE));
                 Debug.Assert(TestGroupNext(Classname: "PowerSupplies"));
                 Debug.Assert(MethodPrior(Name: NONE));
@@ -102,9 +102,9 @@
             Debug.Assert(MethodCustom(Name: "MSO_3014", Description: "Tektronix MSO-3014 Mixed-Signal Oscilloscopes.", CancelNotPassed: "false"));
             if (Data.testSequence.IsOperation) {
                 Debug.Assert(TestGroupPrior(Classname: "DigitalMultiMeters"));
-                Debug.Assert(TestGroupNext(Classname: NONE));
+                Debug.Assert(TestGroupNext(Classname: "WaveformGenerators"));
                 Debug.Assert(MethodPrior(Name: "MM_34401A"));
-                Debug.Assert(MethodNext(Name: NONE));
+                Debug.Assert(MethodNext(Name: "WG_33120A"));
             } else {
                 Debug.Assert(TestGroupPrior(Classname: NONE));
                 Debug.Assert(TestGroupNext(Classname: NONE));
@@ -116,6 +116,32 @@
             return TestIndices.Method.LogFetchAndClear();
         }
     }
+
+    internal class WaveformGenerators {
+        internal static String WG_33120A() {
+            Debug.Assert(TestGroup(Classname: "WaveformGenerators", Description: "Keysight 33120As.", CancelNotPassed: "false", Independent: "true", Methods: "WG_33120A"));
+            Debug.Assert(MethodCustom(Name: "WG_33120A", Description: "Keysight 33120A 15MHz Function/Arbitrary Waveform Generators.", CancelNotPassed: "false"));
+            if (Data.testSequence.IsOperation) {
+                Debug.Assert(TestGroupPrior(Classname: "Oscilloscopes"));
+                Debug.Assert(TestGroupNext(Classname: NONE));
+                Debug.Assert(MethodPrior(Name: "MSO_3014"));
+                Debug.Assert(MethodNext(Name: NONE));
+            } else {
+                Debug.Assert(TestGroupPrior(Classname: NONE));
+                Debug.Assert(TestGroupNext(Classname: NONE));
+                Debug.Assert(MethodPrior(Name: NONE));
+                Debug.Assert(MethodNext(Name: NONE));
+            }
+
+            TestIndices.Method.Event = DiagnosticsT<SCPI_NET>();
+            if (TestIndices.Method.Event != EVENTS.INFORMATION) { // No SCPI_NET instruments defined in TestPlanDefinition.xml.
+                IA.WG.Transport.Command.Invoke("APPLy:SQUare 10E+6, 5.0, -2.5");
+                _ = MessageBox.Show("Press OK to continue.", "Waveform Generator", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+            }
+            return TestIndices.Method.LogFetchAndClear();
+        }
+    }
+
 
     internal static class TestMethods {
         internal static EVENTS DiagnosticsT<T>() where T : IDiagnostics {
