b8709c0d1a66036c6648b2ee6c522bd9ad9c280a
Phillip Smelt <phils@borisch.com>
2025-03-21 12:03:57 -0400

    3/21/25

--- TestPlan/InstrumentsTestPlan.cs
+++ TestPlan/InstrumentsTestPlan.cs
@@ -2,101 +2,75 @@
     using System;
     using System.Collections.Generic;
     using System.Diagnostics;
-    using System.Linq;
     using System.Windows.Forms;
     using ABT.Test.TestLib;
-    using ABT.Test.TestPlans.Diagnostics.InstrumentDrivers;
+    using ABT.Test.TestLib.Configuration;
     using ABT.Test.TestLib.InstrumentDrivers.Generic;
-    using ABT.Test.TestLib.InstrumentDrivers.Interfaces;
     using ABT.Test.TestLib.InstrumentDrivers.Multifunction;
     using ABT.Test.TestLib.InstrumentDrivers.MultiMeters;
     using ABT.Test.TestLib.InstrumentDrivers.Oscilloscopes;
     using ABT.Test.TestLib.InstrumentDrivers.PowerSupplies;
-    using ABT.Test.TestLib.Configuration;
-    using static ABT.Test.TestLib.Data;
-    using static ABT.Test.TestLib.Configuration.Assertions;
-    using static ABT.Test.TestPlans.Diagnostics.TestPlan.InstrumentsTestPlan.TestMethods;
+    using ABT.Test.TestPlans.Diagnostics.InstrumentDrivers;
 
     internal class SwitchingUnits {
         internal static String MSMU_34980A() {
-            if (Data.testSequence.IsOperation) Debug.Assert(TestOperation(NamespaceTrunk: "InstrumentsTestPlan", ProductionTest: "true", Description: "Manufacturer\'s self-tests + ABT tests of TestPlanDefinition\'s InstrumentsTestPlan.", TestGroups: "SwitchingUnits|PowerSupplies|DigitalMultiMeters|Oscilloscopes|WaveformGenerators"));
-            Debug.Assert(TestGroup(Classname: "SwitchingUnits", Description: "Keysight 34980As.", CancelNotPassed: "false", Independent: "true", Methods: "MSMU_34980A"));
-            Debug.Assert(MethodCustom(Name: "MSMU_34980A", Description: "Keysight 34980A Multifunction Switch/Measurement Units.", CancelNotPassed: "false"));
+            if (Data.testSequence.IsOperation) Debug.Assert(Assertions.TestOperation(NamespaceTrunk: "InstrumentsTestPlan", ProductionTest: "true", Description: "Manufacturer\'s self-tests + ABT tests of TestPlanDefinition\'s InstrumentsTestPlan.", TestGroups: "SwitchingUnits|PowerSupplies|DigitalMultiMeters|Oscilloscopes|WaveformGenerators"));
+            Debug.Assert(Assertions.TestGroup(Classname: "SwitchingUnits", Description: "Keysight 34980As.", CancelNotPassed: "false", Independent: "true", Methods: "MSMU_34980A"));
+            Debug.Assert(Assertions.MethodCustom(Name: "MSMU_34980A", Description: "Keysight 34980A Multifunction Switch/Measurement Units.", CancelNotPassed: "false"));
 
-            TestIndices.Method.Event = DiagnosticsT<MSMU_34980A_SCPI_NET>(((MethodCustom)TestIndices.Method).Parameters);
+            TestIndices.Method.Event = TestMethods.DiagnosticsT<MSMU_34980A_SCPI_NET>(((MethodCustom)TestIndices.Method).Parameters);
             return TestIndices.Method.LogFetchAndClear();
         }
     }
 
     internal class PowerSupplies {
         internal static String PS_E3634A() {
-            Debug.Assert(TestGroup(Classname: "PowerSupplies", Description: "Keysight E3634As & E3649As.", CancelNotPassed: "false", Independent: "true", Methods: "PS_E3634A|PS_E3649A"));
-            Debug.Assert(MethodCustom(Name: "PS_E3634A", Description: "Keysight E3634A Power Supplies.", CancelNotPassed: "false"));
+            Debug.Assert(Assertions.TestGroup(Classname: "PowerSupplies", Description: "Keysight E3634As & E3649As.", CancelNotPassed: "false", Independent: "true", Methods: "PS_E3634A|PS_E3649A"));
+            Debug.Assert(Assertions.MethodCustom(Name: "PS_E3634A", Description: "Keysight E3634A Power Supplies.", CancelNotPassed: "false"));
 
-            TestIndices.Method.Event = DiagnosticsT<PS_E3634A_SCPI_NET>(((MethodCustom)TestIndices.Method).Parameters);
+            TestIndices.Method.Event = TestMethods.DiagnosticsT<PS_E3634A_SCPI_NET>(((MethodCustom)TestIndices.Method).Parameters);
             return TestIndices.Method.LogFetchAndClear();
         }
 
         internal static String PS_E3649A() {
-            Debug.Assert(MethodCustom(Name: "PS_E3649A", Description: "Keysight E3649A Power Supplies.", CancelNotPassed: "false"));
+            Debug.Assert(Assertions.MethodCustom(Name: "PS_E3649A", Description: "Keysight E3649A Power Supplies.", CancelNotPassed: "false"));
 
-            TestIndices.Method.Event = DiagnosticsT<PS_E3649A_SCPI_NET>(((MethodCustom)TestIndices.Method).Parameters);
+            TestIndices.Method.Event = TestMethods.DiagnosticsT<PS_E3649A_SCPI_NET>(((MethodCustom)TestIndices.Method).Parameters);
             return TestIndices.Method.LogFetchAndClear();
         }
     }
 
     internal class DigitalMultiMeters {
         internal static String MM_34401A() {
-            Debug.Assert(TestGroup(Classname: "DigitalMultiMeters", Description: "Keysight 34401As.", CancelNotPassed: "false", Independent: "true", Methods: "MM_34401A"));
-            Debug.Assert(MethodCustom(Name: "MM_34401A", Description: "Keysight 34401A Digital Multi-Meters.", CancelNotPassed: "false"));
+            Debug.Assert(Assertions.TestGroup(Classname: "DigitalMultiMeters", Description: "Keysight 34401As.", CancelNotPassed: "false", Independent: "true", Methods: "MM_34401A"));
+            Debug.Assert(Assertions.MethodCustom(Name: "MM_34401A", Description: "Keysight 34401A Digital Multi-Meters.", CancelNotPassed: "false"));
 
-            TestIndices.Method.Event = DiagnosticsT<MM_34401A_SCPI_NET>(((MethodCustom)TestIndices.Method).Parameters);
+            TestIndices.Method.Event = TestMethods.DiagnosticsT<MM_34401A_SCPI_NET>(((MethodCustom)TestIndices.Method).Parameters);
             return TestIndices.Method.LogFetchAndClear();
         }
     }
 
     internal class Oscilloscopes {
         internal static String MSO_3014() {
-            Debug.Assert(TestGroup(Classname: "Oscilloscopes", Description: "Tektronix MSO-3014s.", CancelNotPassed: "false", Independent: "true", Methods: "MSO_3014"));
-            Debug.Assert(MethodCustom(Name: "MSO_3014", Description: "Tektronix MSO-3014 Mixed-Signal Oscilloscopes.", CancelNotPassed: "false"));
+            Debug.Assert(Assertions.TestGroup(Classname: "Oscilloscopes", Description: "Tektronix MSO-3014s.", CancelNotPassed: "false", Independent: "true", Methods: "MSO_3014"));
+            Debug.Assert(Assertions.MethodCustom(Name: "MSO_3014", Description: "Tektronix MSO-3014 Mixed-Signal Oscilloscopes.", CancelNotPassed: "false"));
 
-            TestIndices.Method.Event = DiagnosticsT<MSO_3014_IVI_COM>(((MethodCustom)TestIndices.Method).Parameters);
+            TestIndices.Method.Event = TestMethods.DiagnosticsT<MSO_3014_IVI_COM>(((MethodCustom)TestIndices.Method).Parameters);
             return TestIndices.Method.LogFetchAndClear();
         }
     }
 
     internal class WaveformGenerators {
         internal static String WG_33120A() {
-            Debug.Assert(TestGroup(Classname: "WaveformGenerators", Description: "Keysight 33120As.", CancelNotPassed: "false", Independent: "true", Methods: "WG_33120A"));
-            Debug.Assert(MethodCustom(Name: "WG_33120A", Description: "Keysight 33120A 15MHz Function/Arbitrary Waveform Generators.", CancelNotPassed: "false"));
+            Debug.Assert(Assertions.TestGroup(Classname: "WaveformGenerators", Description: "Keysight 33120As.", CancelNotPassed: "false", Independent: "true", Methods: "WG_33120A"));
+            Debug.Assert(Assertions.MethodCustom(Name: "WG_33120A", Description: "Keysight 33120A 15MHz Function/Arbitrary Waveform Generators.", CancelNotPassed: "false"));
 
-            TestIndices.Method.Event = DiagnosticsT<SCPI_NET>(((MethodCustom)TestIndices.Method).Parameters);
+            TestIndices.Method.Event = TestMethods.DiagnosticsT<SCPI_NET>(((MethodCustom)TestIndices.Method).Parameters);
             if (TestIndices.Method.Event != EVENTS.INFORMATION) { // No SCPI_NET instruments defined in TestPlanDefinition.xml.
                 IA.WG.Transport.Command.Invoke("APPLy:SQUare 10E+6, 5.0, -2.5");
                 _ = MessageBox.Show("Press OK to continue.", "Waveform Generator", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
             }
             return TestIndices.Method.LogFetchAndClear();
-        }
-    }
-
-
-    internal static class TestMethods {
-        internal static EVENTS DiagnosticsT<T>(List<Parameter> Parameters) where T : IDiagnostics {
-            Dictionary<String, T> instrumentDriversT = Data.InstrumentDrivers.Where(kvp => kvp.Value is T).ToDictionary(kvp => kvp.Key, kvp => (T)kvp.Value);
-            if (instrumentDriversT.Count() == 0) {
-                TestIndices.Method.Log.AppendLine($"No instruments of type '{typeof(T).Name}' defined in '{TestExecDefinitionXML}'.");
-                return EVENTS.INFORMATION;
-            }
-
-            (Boolean Summary, List<DiagnosticsResult> Details) resultDiagnostics;
-            Boolean passedCollective = true;
-            foreach (KeyValuePair<String, T> kvp in instrumentDriversT) {
-                resultDiagnostics = kvp.Value.Diagnostics(Parameters);
-                passedCollective &= resultDiagnostics.Summary;
-                TestIndices.Method.Log.AppendLine($"ID '{kvp.Key}', Driver '{typeof(T).Name}', Result '{(resultDiagnostics.Summary ? EVENTS.PASS.ToString() : EVENTS.FAIL.ToString())}'.");
-                foreach (DiagnosticsResult dr in resultDiagnostics.Details) TestIndices.Method.Log.AppendLine($"{dr.Label}{dr.Message}, Result '{dr.Event}'.");
-            }
-            return passedCollective ? EVENTS.PASS : EVENTS.FAIL;
         }
     }
 }
---
+++ TestPlan/TestMethods.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ABT.Test.TestLib.Configuration;
+using ABT.Test.TestLib.InstrumentDrivers.Interfaces;
+using ABT.Test.TestLib;
+using System.Text;
+namespace ABT.Test.TestPlans.Diagnostics.TestPlan {
+
+    internal static class TestMethods {
+        internal static EVENTS DiagnosticsT<T>(List<Parameter> Parameters) where T : IDiagnostics {
+            Dictionary<String, T> instrumentDriversT = Data.InstrumentDrivers.Where(kvp => kvp.Value is T).ToDictionary(kvp => kvp.Key, kvp => (T)kvp.Value);
+            if (instrumentDriversT.Count() == 0) {
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder.AppendLine($"No instruments of type '{typeof(T).Name}' found:");
+                stringBuilder.AppendLine($"Check '{Data.TestExecDefinitionXML}' for missing:");
+                stringBuilder.AppendLine($" - Element '{nameof(InstrumentTestExec)}'.");
+                stringBuilder.AppendLine($" - Attribute '{nameof(InstrumentTestExec.NameSpacedClassName)}' = '{typeof(T).Name}'.");
+                stringBuilder.AppendLine(String.Empty);
+                stringBuilder.AppendLine($"Check '{Data.TestPlanDefinitionXML}' for:");
+                stringBuilder.AppendLine($" - Element '{nameof(TestSpace)}'.");
+                stringBuilder.AppendLine($" - Attribute '{nameof(TestSpace.Simulate)}' = '{true}'.");
+                TestIndices.Method.Log.Append(stringBuilder.ToString());
+                return EVENTS.INFORMATION;
+            }
+
+            (Boolean Summary, List<DiagnosticsResult> Details) resultDiagnostics;
+            Boolean passedCollective = true;
+            foreach (KeyValuePair<String, T> kvp in instrumentDriversT) {
+                resultDiagnostics = kvp.Value.Diagnostics(Parameters);
+                passedCollective &= resultDiagnostics.Summary;
+                TestIndices.Method.Log.AppendLine($"ID '{kvp.Key}', Driver '{typeof(T).Name}', Result '{(resultDiagnostics.Summary ? EVENTS.PASS.ToString() : EVENTS.FAIL.ToString())}'.");
+                foreach (DiagnosticsResult dr in resultDiagnostics.Details) TestIndices.Method.Log.AppendLine($"{dr.Label}{dr.Message}, Result '{dr.Event}'.");
+            }
+            return passedCollective ? EVENTS.PASS : EVENTS.FAIL;
+        }
+    }
+}
