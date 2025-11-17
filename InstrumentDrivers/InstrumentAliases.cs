770e9053b02ad87abc29b1ac4402f64ed7dab208
Phillip Smelt <98059794+Phillip-Smelt@users.noreply.github.com>
2025-11-17 16:41:56 -0500

    11/17/25

--- InstrumentDrivers/InstrumentAliases.cs
+++ InstrumentDrivers/InstrumentAliases.cs
@@ -8,9 +8,9 @@ namespace ABT.Test.TestPlans.Diagnostics.InstrumentDrivers {
     using System;
 
     internal class InstrumentDriverInstances {
-        //internal MSMU_34980A_SCPI_NET MSMU = ((MSMU_34980A_SCPI_NET)(TestLib.InstrumentDrivers["MSMU1_34980A"]));
-        //internal PS_E3649A_SCPI_NET P12V_N12V = ((PS_E3649A_SCPI_NET)(TestLib.InstrumentDrivers["PS1ε2_E3649A"]));
-        //internal PS_E3634A_SCPI_NET P28V = ((PS_E3634A_SCPI_NET)(TestLib.InstrumentDrivers["PS3_E3634A"]));
+        internal MSMU_34980A_SCPI_NET MSMU = ((MSMU_34980A_SCPI_NET)(TestLib.InstrumentDrivers["MSMU1_34980A"]));
+        internal PS_E3649A_SCPI_NET P12V_N12V = ((PS_E3649A_SCPI_NET)(TestLib.InstrumentDrivers["PS1ε2_E3649A"]));
+        internal PS_E3634A_SCPI_NET P28V = ((PS_E3634A_SCPI_NET)(TestLib.InstrumentDrivers["PS3_E3634A"]));
         internal MSO_3014_IVI_COM MSO = ((MSO_3014_IVI_COM)(TestLib.InstrumentDrivers["MSO1_3014"]));
         //internal MM_34401A_SCPI_NET MM = ((MM_34401A_SCPI_NET)(TestLib.InstrumentDrivers["MM1_34401A_SCPI_NET"]));
         //internal SCPI_NET WG = ((SCPI_NET)(TestLib.InstrumentDrivers["WG1_33120A"]));
