d6268c1f460e15e7126695106073d9b0bff9c015
Phillip Smelt <98059794+Phillip-Smelt@users.noreply.github.com>
2025-12-10 08:58:14 -0500

    12/10/25

--- InstrumentDrivers/InstrumentDriverInstances.cs
+++ InstrumentDrivers/InstrumentDriverInstances.cs
@@ -7,9 +7,9 @@ namespace ABT.Test.TestPlans.Diagnostics.InstrumentDrivers {
 
     internal class InstrumentDriverInstances {
         internal MSMU_34980A_SCPI_NET MSMU = ((MSMU_34980A_SCPI_NET)(TestLib.InstrumentDrivers["MSMU1_34980A"]));
-        internal PS_E3649A_SCPI_NET P12V_N12V = ((PS_E3649A_SCPI_NET)(TestLib.InstrumentDrivers["PS1ε2_E3649A"]));
-        internal PS_E3634A_SCPI_NET P28V = ((PS_E3634A_SCPI_NET)(TestLib.InstrumentDrivers["PS3_E3634A"]));
         internal MSO_3014_VISA_NET MSO = ((MSO_3014_VISA_NET)(TestLib.InstrumentDrivers["MSO1_3014"]));
+        internal PS_E3634A_SCPI_NET P28V = ((PS_E3634A_SCPI_NET)(TestLib.InstrumentDrivers["PS3_E3634A"]));
+        internal PS_E3649A_SCPI_NET P12V_N12V = ((PS_E3649A_SCPI_NET)(TestLib.InstrumentDrivers["PS1ε2_E3649A"]));
         //internal MM_34401A_SCPI_NET MM = ((MM_34401A_SCPI_NET)(TestLib.InstrumentDrivers["MM1_34401A_SCPI_NET"]));
         //internal SCPI_NET WG = ((SCPI_NET)(TestLib.InstrumentDrivers["WG1_33120A"]));
     }
